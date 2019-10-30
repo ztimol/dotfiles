4db10f0d7774b054924793f9febdb6f8e3bc19cd
zaheer <ztimol@gmail.com>
2019-10-30 11:40:20 +0200

    added more bash files

--- emacs/emacs.el
+++ emacs/emacs.el
@@ -38,7 +38,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(org-agenda-files nil)
- '(package-selected-packages '(wgrep-ag wgrep ag use-package)))
+ '(package-selected-packages (quote (php-mode wgrep-ag wgrep ag use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
